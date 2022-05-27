1549517ac7a98817908c4dc5cb0be279d90971c7
arigato <teeruch.k@gmail.com>
2022-05-27 20:08:29 +0700

    use template

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
-  styleUrls: ['./heros.component.scss']
+  styleUrls: ['./heros.component.scss'],
 })
 export class HerosComponent implements OnInit {
+  hero = 'bas เวลานอน มรณะ';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.hero = this.hero + '🤣';
   }
-
 }
