9e890bdfa4e578f052d9d9ed8b66f2415f0b6097
arigato <teeruch.k@gmail.com>
2022-05-28 10:11:43 +0700

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, HerosComponent],
+  declarations: [AppComponent, HerosComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
